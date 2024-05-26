c2ff1b949300050d46b7f2efd7bcff172747d675
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-05-27 00:58:47 +0530

    Time: 65 ms (84.83%), Space: 20.8 MB (56.06%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,45 @@
 class Solution {
-    bool subsetsum(vector<int>& arr, int sum) {
-        int n = arr.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;  // A sum of 0 is always possible
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (arr[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+    bool subsetsum(vector<int>&arr,int sum){
+        int n=arr.size();
+        int dp[n+1][sum+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0) dp[i][j]=false;
+                if(j==0) dp[i][j]=true;
             }
         }
-
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j){
+                        dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                    
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            
+            }
+        }
         return dp[n][sum];
+        
+        
+        
     }
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
         }
-        if (sum % 2 != 0) {
-            return false;
-        }
-
-        return subsetsum(nums, sum / 2);
+        if(sum%2!=0) return false;
+   return subsetsum(nums,sum/2);
+        
+        
     }
 };
