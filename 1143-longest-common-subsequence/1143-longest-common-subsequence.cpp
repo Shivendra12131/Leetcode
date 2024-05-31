2319f2fe6ced29678646e22c55dc707f452ac4f2
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-05-31 16:04:50 +0530

    Time: 22 ms (66.67%), Space: 25.6 MB (51.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,23 +2,46 @@ class Solution {
 private:
     int longestCommonSubsequence(string& text1, string& text2, int n, int m, vector<vector<int>>& dp) {
         //memoization 
-        if (n == 0 || m == 0) return 0;
-        if (dp[n][m] != -1) return dp[n][m];
+//         if (n == 0 || m == 0) return 0;
+//         if (dp[n][m] != -1) return dp[n][m];
 
-        if (text1[n-1] == text2[m-1]) {
-            dp[n][m] = 1 + longestCommonSubsequence(text1, text2, n-1, m-1, dp);
-        } else {
-            dp[n][m] = max(longestCommonSubsequence(text1, text2, n-1, m, dp), 
-                           longestCommonSubsequence(text1, text2, n, m-1, dp));
+//         if (text1[n-1] == text2[m-1]) {
+//             dp[n][m] = 1 + longestCommonSubsequence(text1, text2, n-1, m-1, dp);
+//         } else {
+//             dp[n][m] = max(longestCommonSubsequence(text1, text2, n-1, m, dp), 
+//                            longestCommonSubsequence(text1, text2, n, m-1, dp));
+//         }
+//         return dp[n][m];
+//     }
+        
+        //top down approach
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+                
+            }
         }
-        return dp[n][m];
+        return dp[n][m];     
     }
 
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=0;i<n+1;i++)
+        {
+            dp[i][0]=0;
+        }
+        
         return longestCommonSubsequence(text1, text2, n, m, dp);
     }
 };
