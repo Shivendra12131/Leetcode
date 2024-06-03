3f0f322789a73811da77be1b07f5fa3e525a6ea4
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-06-03 16:58:43 +0530

    Time: 14 ms (92.76%), Space: 12 MB (94.12%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     string lcs(string str1,string str2,int n,int m){
         int t[n+1][m+1];
-        
-        
         for(int i=0;i<=n;i++){  // initialization
             for(int j=0;j<=m;j++ ){
                 if(i==0 || j==0){
@@ -24,58 +22,45 @@ public:
                 }
             }
         }
+        // yaha tak longest common subseuqence ban gya hai
+        
+       //-> // ab longest common subsequence jo hai usko nikalo 
+        
         int i=n;int j=m;
-        string s="";
+        string s="";//empty string
+        
         while(i>0 && j>0){
-            
             if(str1[i-1]==str2[j-1]){
-                
                 s+=str1[i-1];
                 i--;
                 j--;
             }
             else{
-                
                 if(t[i-1][j]<t[i][j-1]){
-                    
                     s+=str2[j-1];
                     j--;
                 }
-                else{
-                    
+                else{       
                     s+=str1[i-1];
                     i--;
                 }
-            }
-            
-         
-            
-            
-            
-            
-           
+            }  
         }
-        
+        // ab longest common subsequence nikalne ke baad jo bacha hua hai dono me usko string me bhar do
+        // aur uske baad return kar do string ko
         
         while(i>0){
-             
              s+=str1[i-1];
              i--;
-             
          } 
          while(j>0){
              s+=str2[j-1];
-             j--;
-             
-             
+             j--;     
          }
          reverse(s.begin(),s.end());
-        
         return s;
-        
- 
     }
-
+// main function
     string shortestCommonSupersequence(string str1, string str2) {
         int n=str1.length();
         int m=str2.length();
