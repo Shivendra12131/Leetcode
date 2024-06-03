ef29bf653322e0b7a27cf6b829becc04c08dd5c0
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-06-03 14:50:45 +0530

    Time: 31 ms (49.49%), Space: 29.6 MB (48.39%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -6,11 +6,13 @@ public:
         int n=s.length();
         int m=n;
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        int plength=0;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++){
                 if(s[i-1]==b[j-1])
                 {
+                    
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
@@ -18,6 +20,7 @@ public:
                 }
             }
         }
+
         return n-dp[n][m];
     }
 };
