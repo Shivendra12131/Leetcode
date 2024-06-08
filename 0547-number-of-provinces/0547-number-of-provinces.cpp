cc261f7476a87cd9e777ca5b47669e4e1371669b
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-06-08 16:51:45 +0530

    Time: 28 ms (15.25%), Space: 19.4 MB (34.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -17,20 +17,20 @@ public:
         int n = isConnected.size();
         
         // Make an adjacency list
-        vector<int> adj[n];
+        vector<int> adj[n+1];
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                if (isConnected[i][j] == 1 ) {
+                    adj[i+1].push_back(j+1);
+                    adj[j+1].push_back(i+1);
                 }
             }
         }
-        
-        vector<int> vis(n, 0);
+
+        vector<int> vis(n+1, 0);
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (vis[i] == 0) {
                 cnt++;
                 dfs(adj, i, vis);
