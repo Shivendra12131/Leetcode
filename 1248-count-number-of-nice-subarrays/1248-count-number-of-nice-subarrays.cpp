a3a3495a8ee8e5e2d1a3a860cad6005efa8526d5
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-06-23 03:03:46 +0530

    Time: 310 ms (5.05%), Space: 109.2 MB (6.01%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        queue<int> oddIndices;
-        int subarrays = 0, lastPopped = -1, initialGap = -1;
-
-        for (int i = 0; i < nums.size(); i++) {
-            // If element is odd, push it in queue.
-            if (nums[i] % 2 == 1) {
-                oddIndices.push(i);
-            }
-            // If queue size exceeds k, perform pop operation.
-            if (oddIndices.size() > k) {
-                lastPopped = oddIndices.front();
-                oddIndices.pop();
-            }
-            // While the size of queue is k, add the even numbers in the
-            // beginning of subarray to answer.
-            if (oddIndices.size() == k) {
-                initialGap = oddIndices.front() - lastPopped;
-                subarrays += initialGap;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if((nums[i]&1)==1) nums[i]=1;
+            else{
+                nums[i]=0;
             }
         }
-
-        return subarrays;
+    
+  int prefix_sum=0;
+        map<int,int>mpp;
+        mpp[0]=1;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            prefix_sum+=nums[i];
+            int remove=prefix_sum-k;
+            cnt+=mpp[remove];
+            mpp[prefix_sum]+=1;
+            
+        }
+    
+   
+       return cnt; 
     }
 };
